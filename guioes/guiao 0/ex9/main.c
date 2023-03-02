922a7da81876dc055db292a8168765754764ed37
Mauro Lopes <mtomasclopes@gmail.com>
2023-03-02 17:52:47 +0000

    ex9 atualizado

--- guioes/guiao 0/ex9/main.c
+++ guioes/guiao 0/ex9/main.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
-
+#define N 3
 
 //praticamente a mesma coisa
 
-int nomeSuperOriginalParaUmaFuncaoDoExercicioNoveDoGuiaoZeroYey(int m[][3], int N){
+int nomeSuperOriginalParaUmaFuncaoDoExercicioNoveDoGuiaoZeroYey(int m[][3]){
 
     for(int i=0; i<N ; i++){
         for(int j=0; j<3; j++){
             printf("%d ", m[i][j]);
         }
+        putchar('\n');
     }
     return 0;
 }
 
-
-
-
-
-
-
-
-
 int main() {
-    printf("Hello, World!\n");
+    int m[3][N]={{1,2,3}, {4,5,6}, {7,8,9}};
+    nomeSuperOriginalParaUmaFuncaoDoExercicioNoveDoGuiaoZeroYey(m);
     return 0;
 }
