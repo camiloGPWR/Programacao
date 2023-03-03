ed3afd446c75cc9824f6b7e5bd528bc8a932a589
Mauro Lopes <mtomasclopes@gmail.com>
2023-03-03 11:55:24 +0000

    Pasta do guiao 1 adicionada
    sem exercicios resolvidos

---
+++ guioes/guiao 1/ex19/cmake-build-debug/CMakeFiles/ex19.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex19.dir/main.c.obj"
+  "ex19.exe"
+  "ex19.exe.manifest"
+  "ex19.pdb"
+  "libex19.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
