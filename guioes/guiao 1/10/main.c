87f8ec1c6731d59513986f1ee491db1bba6f019f
Mauro Lopes <mtomasclopes@gmail.com>
2023-03-09 18:44:08 +0000

    ex10 adicionado

---
+++ guioes/guiao 1/10/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int tres_consecutivos(char *frase){
+
+    for(int i=2; *(frase+i)!='\0'; i++){
+        if(*(frase+i)==*(frase+i-1) && *(frase+i-1)==*(frase+i-2)){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+int main() {
+    char frase[80]="ola meus caros e caraaas";
+    printf("%d", tres_consecutivos(frase));
+    return 0;
+}
